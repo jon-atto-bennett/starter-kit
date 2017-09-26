afaa59f00c7ca5a55257a9fd2e38ffc1edbfd51b
jon-atto-bennett <jon.bennett@outlook.co.nz>
2017-09-26 16:20:55 +1300

    added HTTP calling with mocking

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -13,11 +13,20 @@ const compiler = webpack(config)
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
   publicPath: config.output.publicPath
-}))
+}));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'))
-})
+});
+
+app.get('/users', function (req, res) {
+  // Hard coding for simplicity. Pretend this hits a real database
+  res.json([
+    {"id": 1, "firstName": "Bob", "lastName": "Smith", "email": "bob@gmail.com"},
+    {"id": 2, "firstName": "Tammy", "lastName": "Norton", "email": "tammy@gmail.com"},
+    {"id": 3, "firstName": "Tina", "lastName": "Lee", "email": "tina@gmail.com"}
+  ]);
+});
 
 app.listen(port, function (err) {
   if (err) {
@@ -25,4 +34,4 @@ app.listen(port, function (err) {
   } else {
     open('http://localhost:' + port)
   }
-})
+});
